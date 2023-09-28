c49e432cf5f5cb0a117b1bc26641ad7d96c01f3b
DanVicenteIhanus <dan.vicente.ihanus@gmail.com>
2023-09-28 21:56:55 +0200

    edit project_svm

--- python/project_svm.py
+++ python/project_svm.py
@@ -3,13 +3,16 @@ import numpy as np
 from matplotlib import pyplot as plt
 import scipy.stats as st
 import sklearn as sk
-
+import os
 
 def import_data(datadir):
     data = pd.read_csv(datadir)
     return data
 
 def main():
-    datadir='...'
-    data = import_data(datadir)
-    
+    datadir = '/Users/danvicente/Statistik/SF2935\ -\ Modern\ Methods/Project/data'
+    file = 'project_train.csv'
+    data = import_data(datadir+file)
+    print(data)
+
+main()
